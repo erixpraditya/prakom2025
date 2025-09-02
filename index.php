b752f383eceede20fbfd8882ee9fa1caf4f6d696
erixpraditya <erixpradityaa@gmail.com>
2025-09-02 20:42:36 +0700

    update tampilan

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="css/bootstrap.css">
+    <title>Klinik Sehat</title>
+</head>
+<body style="background-color: #DDF4E7;">
+    <?php
+      include('navbar.php');
+    ?>
+    <div class="container">
+      <!-- Konten nya -->
+      <div class="row">
+        <div class="col-10 m-auto mt-5">
+          <div class="card">
+              <div class="card-header">
+                Klinik Sehat
+              </div>
+              <div class="card-body">
+                  <h5 class="card-title">Hallo</h5>
+                  <p class="card-text">Selamat datang di aplikasi klinik Sehat.</p>
+              </div>
+        </div>
+        </div>
+      </div>
+    </div>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>
+</body>
+</html>
