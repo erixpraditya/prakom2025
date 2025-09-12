db8bbf6672f00653c035b9555702254930254a11
erixpraditya <erixpradityaa@gmail.com>
2025-09-12 18:33:15 +0700

    Hapus dokter

--- dokter/index.php
+++ dokter/index.php
@@ -26,10 +26,8 @@
                             <thead>
                                 <tr>
                                     <th scope="col">No</th>
-                                    <th scope="col">Nama Pasien</th>
-                                    <th scope="col">Tanggal Lahir</th>
-                                    <th scope="col">Jenis Kelamin</th>
-                                    <th scope="col">Alamat</th>
+                                    <th scope="col">Nama Dokter</th>
+                                    <th scope="col">Nama Poli</th>
                                     <th scope="col">Aksi</th>
                                 </tr>
                             </thead>
@@ -39,7 +37,7 @@
                                 include('../koneksi.php');
 
                                 #2. Menuliskan query
-                                $qry = "SELECT * FROM pasien";
+                                $qry = "SELECT * FROM dokter INNER JOIN poli ON dokter.Poli_ID = poli.Poli_ID";
 
                                 #3. Menjalankan query
                                 $result = mysqli_query(mysql: $koneksi, query: $qry);
@@ -47,23 +45,20 @@
                                 #4. Melakukan Looping data pasien
                                 $nomor = 1;
                                 foreach ($result as $row) {
-                                    $tgl_lahir = date_create(datetime: $row['Tanggal_LahirPasien']);
-                                    $tgl_lahir = date_format(object: $tgl_lahir, format: 'D, d F Y')
+                                    
                                 ?>
                                     <tr>
                                         <th scope="row"><?= $nomor++ ?></th>
-                                        <td><?= $row['Nama_pasienKlinik'] ?></td>
-                                        <td><?= $tgl_lahir ?></td>
-                                        <td><?= $row['Jenis_KelaminPasien'] ?></td>
-                                        <td><?= $row['Alamat_Pasien'] ?></td>
+                                        <td><?= $row['Nama_Dokter'] ?></td>
+                                        <td><?= $row['Nama_Poli'] ?></td>
                                         <td>
-                                            <a href="edit.php?id=<?= $row['pasienKlinik_ID'] ?>" class="btn btn-info btn-sm">Edit</a>
+                                            <a href="edit.php?id=<?= $row['Dokter_ID'] ?>" class="btn btn-info btn-sm">Edit</a>
                                             <!-- Button trigger modal -->
-                                            <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal<?= $row['Nama_pasienKlinik'] ?>">
+                                            <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal<?= $row['Dokter_ID'] ?>">
                                                 Hapus
                                             </button>
                                             <!-- Modal -->
-                                            <div class="modal fade" id="exampleModal<?= $row['Nama_pasienKlinik'] ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                            <div class="modal fade" id="exampleModal<?= $row['Dokter_ID'] ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                                 <div class="modal-dialog">
                                                     <div class="modal-content">
                                                         <div class="modal-header">
@@ -71,11 +66,11 @@
                                                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                         </div>
                                                         <div class="modal-body">
-                                                            Yakin data pasien <b><?= $row['Nama_pasienKlinik'] ?></b> akan dihapus?
+                                                            Yakin data pasien <b><?= $row['Nama_Dokter'] ?></b> akan dihapus?
                                                         </div>
                                                         <div class="modal-footer">
                                                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
-                                                            <a href="hapus.php?id=<?= $row['pasienKlinik_ID'] ?>" class="btn btn-danger">Hapus</a>
+                                                            <a href="hapus.php?id=<?= $row['Dokter_ID'] ?>" class="btn btn-danger">Hapus</a>
                                                         </div>
                                                     </div>
                                                 </div>
